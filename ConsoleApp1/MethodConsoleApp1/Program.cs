9d01db8be9aac570e3a893dd3eea55cf9bb7d10d
DuyghuAbdullayeva <abdullayevaduyqu3@gmail.com>
2024-07-30 16:06:04 +0400

    Add and Subtract

---
+++ ConsoleApp1/MethodConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MethodConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 65;
+            int secondNumber = 42;
+            int AddResault = Add(firstNumber, secondNumber);
+            Console.WriteLine("Add Resault:" + AddResault);
+            float SubtractResault = Subtract(firstNumber, secondNumber);
+            Console.WriteLine("Subtract Resault:"+ SubtractResault);
+        }
+        static int Add(int firstNumber, int secondNumber)
+        {
+            int resault = 0;
+            resault = firstNumber + secondNumber;
+            return resault;
+
+        }
+        static float Subtract(float firstNumber, float secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        
+
+    }
+}
